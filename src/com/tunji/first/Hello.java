15387d5051d5ef6e65d427a1fbf1c2a23db83def
Tunji Akinbami <a.akinbami@gmail.com>
2015-03-03 12:01:49 -0500

    FirstWS

---
+++ src/com/tunji/first/Hello.java
@@ -0,0 +1,32 @@
+package com.tunji.first;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Hello {
+
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayTextHello(){
+		return "Hello Jersey";
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello(){
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+	}
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHTMLHello(){
+		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+        + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+		
+	}
+}
